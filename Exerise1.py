5aeeeb71f606d26400df55c29a6ec588159b1dfc
nncm-xbc <55660406+nncm-xbc@users.noreply.github.com>
2021-06-04 19:06:59 +0200

    Progresseufhemjùock

--- Exerise1.py
+++ Exerise1.py
@@ -1,6 +1,8 @@
 # Import functions and libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import statistics
+import math
 
 from scipy.fftpack import dct, idct
 from numpy import r_
@@ -37,25 +39,98 @@ def idct_apply(image):
 
 
 def watermark(a):
-    counter = 1
-    maxi = 11
+    counter = 0
+    maxi = k
 
-    for i in range(1-a.shape[0], a.shape[0]):
-        diag = np.diagonal(a[::-1, :], i)[::(2 * (i % 2) - 1)]
-        for j in diag:
-            if counter < maxi + 1:
-                index = np.where(a == j)
-                a[index] = j * (1 + 0.02 * (np.random.normal(0, 1, 1)))
-            else:
-                index = np.where(a == j)
-                a[index] = 0
-            counter += 1
-    return a
+    w = np.random.normal(0, 1, k)
+    w_sequence = []
+    for i in w:
+        w_sequence.append(i)
 
 
-image = rgb2gray(imread("Hugo_Simberg-The_Wounded_Angel.jpg").astype(float))
+    size = image.shape
+    for x in r_[:size[0]:8]:
+        for y in r_[:size[1]:8]:
+            for i in range(1-a.shape[0], a.shape[0]):
+                diag = np.diagonal(a[x:(x + 8), y:(y + 8)][::-1, :], i)[::(2 * (i % 2) - 1)]
+                for j in diag:
+                    if counter < maxi:
+                        index = np.where(a == j)
+                        a[index] = j * (1 + 0.02 * (w[counter]))
+
+                    counter += 1
+    return a, w_sequence
+
+
+def klargest(a):
+    list_coefs = []
+
+    list_of_averages = []
+
+    size = a.shape
+    for x in r_[:size[0]:8]:
+        for y in r_[:size[1]:8]:
+            list_coefs_block = []
+            for i in range(1 - a[x:(x + 8), y:(y + 8)].shape[0], a[x:(x + 8), y:(y + 8)].shape[0]):
+                diag = np.diagonal(a[x:(x + 8), y:(y + 8)][::-1, :], i)[::(2 * (i % 2) - 1)]
+                for j in diag:
+                    list_coefs_block.append(j)
+                list_coefs.append(list_coefs_block)
+
+    for i in range(k):
+        sum = 0.0
+        for coefs_block in list_coefs:
+            # print('coefs blocks: ', coefs_block)
+            sum += coefs_block[i]
+        average = sum/len(list_coefs)
+        list_of_averages.append(average)
+        print('Average value: ', average)
+
+    return list_of_averages
+
+
+def estimate_mark(averages_primary, averages_secondary):
+    list_w = []
+    for i in range(k):
+        w = (averages_secondary[i] - averages_primary[i])/0.02 * averages_primary[i]
+        list_w.append(w)
+        print('Wi-k :', w)
+
+    return list_w
+
+
+def gamma(w_sequence, wi_sequence):
+
+    w_mean = statistics.mean(w_sequence)
+    wi_mean = statistics.mean(wi_sequence)
+
+    sum_a = 0
+    sum_b = 0
+    sum_c = 0
+    for i in range(k):
+        sum_a += (wi_sequence[i] - wi_mean)*(w_sequence[i] - w_mean)
+        sum_b += (wi_sequence[i] - wi_mean)**2
+        sum_c += (w_sequence[i] - w_mean)**2
+
+    val_gamma = sum_a/math.sqrt(sum_b * sum_c)
+
+    return val_gamma
+
+
+image = rgb2gray(imread("Hugo_Simberg-The_Wounded_Angel.jpg"))
+
+k = 11
 
 dct_image = dct_apply(image)
+
+idct_image = idct_apply(dct_image)
+
+dct_watermarked, w_sequence = watermark(dct_image)
+
+idct_watermarked = idct_apply(dct_watermarked)
+
+difference_image = image - idct_watermarked
+
 
 # Display the original image in greyscale
 plt.figure(figsize=(15, 10), constrained_layout=False)
@@ -68,22 +143,13 @@ plt.imshow(dct_image, cmap='gray')
 plt.title("2D DCT of the image")
 plt.show()
 
-size = image.shape
-for i in r_[:size[0]:8]:
-    for j in r_[:size[1]:8]:
-        watermark(dct_image[i:(i + 8), j:(j + 8)])
-
-idct_image = idct_apply(dct_image)
-
-difference_image = image - idct_image
-
-# Display DCT image
+# Display DCT watermarked image
 plt.figure()
-plt.imshow(dct_image, cmap='gray')
+plt.imshow(dct_watermarked, cmap='gray')
 plt.title("2D DCT of the image, with watermark")
 plt.show()
 
-# Display IDCT image
+# Display IDCT watermarked image
 plt.figure()
 plt.imshow(idct_image, cmap='gray')
 plt.title("2D IDCT of the image, with watermark")
@@ -105,3 +171,16 @@ plt.show()
 --------------------------PART 2--------------------------
 """
 
+original = image.copy()
+mystery_img = idct_image.copy()
+
+dct_mystery = dct_apply(mystery_img)
+
+averages_mystery = klargest(dct_mystery)
+print('-----------------------')
+average_original = klargest(mystery_img)
+
+wi_sequence = estimate_mark(average_original, averages_mystery)
+
+gamma_val = gamma(w_sequence, wi_sequence)
+print('gamma value  = ', gamma_val)
